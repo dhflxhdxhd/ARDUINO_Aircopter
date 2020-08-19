6dcb52736e841b838a8e9502873f0696b5ba59d7
dhflxhdxhd@gmail.com <dhflxhdxhd@gmail.com>
2020-08-19 15:33:34 +0900

    control speed

---
+++ Aircopter_control_motor/Aircopter_control_motor.ino
@@ -0,0 +1,47 @@
+void setup() {
+  Serial.begin(115200);
+  pinMode(5, OUTPUT);
+}
+
+void loop() {
+  /*
+    if (Serial.available() > 0) {
+    //char value = Serial.read();
+    int value = Serial.parseInt();
+    //Serial.write(49); 아스키 값으로 출력 (binary data)
+    Serial.print(value);
+
+    if (value == 1) {
+      Serial.println("ON");
+    // analogWrite(6, 100);
+    analogWrite(5,50)
+      delay(500);
+    } /*else {
+      Serial.println("OFF");
+      analogWrite(6, 0);
+    }
+    }
+    if (Serial1.available()) {
+    char blueData = Serial1.read();
+    Serial.write(blueData);
+    Serial.println();
+
+    if (blueData == '0')
+    analogWrite(5, 0);
+    }
+  */
+
+  //  control motor speed
+  if (Serial.available() > 0) {
+    char userInput = Serial.read();
+    Serial.println(userInput);
+
+    if (userInput >= '0' && userInput <= '9') {
+      int throttle = (userInput - '0') * 10;
+      analogWrite(6, throttle);
+      analogWrite(10, throttle);
+      analogWrite(9, throttle);
+      analogWrite(5, throttle);
+    }
+  }
+}
